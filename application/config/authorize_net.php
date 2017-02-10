c969cb7caaacb88a186acdfa99ba9a3be0527861
meettomangesh <meettomangesh@gmail.com>
2017-02-10 23:43:21 +0530

    Updated to integrate the payment gateway

---
+++ application/config/authorize_net.php
@@ -0,0 +1,12 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Authorize.net Account Info
+//$config['api_login_id'] = '4SpEa583';
+//$config['api_transaction_key'] = '8qZN33j93cQMNK9a';
+$config['api_login_id'] = '9L59PtBxm4G';
+$config['api_transaction_key'] = '5d2h26m6Fk7HFgJF';
+
+$config['api_url'] = 'https://test.authorize.net/gateway/transact.dll'; // TEST URL
+//$config['api_url'] = 'https://secure.authorize.net/gateway/transact.dll'; // PRODUCTION URL
+
+/* EOF */
